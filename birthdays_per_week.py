02b80160b3f70999f81b2a474b97ecf2eab9fcdb
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-07-28 13:03:06 +0200

    Update birthdays_per_week.py

--- birthdays_per_week.py
+++ birthdays_per_week.py
@@ -22,18 +22,18 @@ def get_birthdays_per_week(users):
         }
 
     current_date = datetime.now()
-    # current_date = datetime(2023, 7, 24) # перевірка, якби сьогодні був понеділок
     interval = timedelta(days=7)
     weekend_interwal = timedelta(days=2)
-    upcoming_date = current_date + interval
     
     # робимо словник, в якому будуть тільки ті люди, у яких ДР протягом наступних 7 днів:    
     
     for name, date in users.items():
         date = date.replace(year=current_date.year)
 
-        if current_date <= date <= upcoming_date:
-            day_of_week_num = datetime.weekday(date)   # вказуємо, що то за день тижня (date з рядку 31)
+        if current_date.date() <= date.date() <= current_date.date() + interval:
+            day_of_week_num = datetime.weekday(date)
+            if day_of_week_num in [5, 6]:
+                birthdays_weekdays_list[0].append(name)   # якщо ДР на вихідних
             birthdays_weekdays_list[day_of_week_num].append(name)
     
         if current_date.weekday() == 0 and current_date - weekend_interwal <= date < current_date: # якщо сьогодні понеділок
@@ -51,9 +51,9 @@ def get_birthdays_per_week(users):
         5: "Saturday",
         6: "Sunday"}
     
-    for day in range(5): # 5, а не 7, бо без наступних вихідних
-        name_of_the_day = dict_with_day_names[day]
-        colleagues_to_greet = birthdays_weekdays_list[day]
+    for day in range(current_date.weekday(), current_date.weekday() + 7): # стартуємо з сьогодні (range(4, 11))
+        name_of_the_day = dict_with_day_names[day % 7] # day % 7 - остача від ділення на 7 (або скільки залишається до найближчого меншого числа, що ділиться на 7 без остачі)
+        colleagues_to_greet = birthdays_weekdays_list[day % 7]
         if colleagues_to_greet:
             print(f"{name_of_the_day}: {', '.join(colleagues_to_greet)}")
 
